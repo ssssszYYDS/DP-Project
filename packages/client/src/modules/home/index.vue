63e966b3522aae9a84f5a29f09d339b8af4c1da5
ssssszYYDS <xiakj@shanghaitech.edu.cn>
2024-05-19 17:00:12 +0800

    init

---
+++ packages/client/src/modules/home/index.vue
@@ -0,0 +1,23 @@
+<template>
+
+</template>
+<script setup>
+import { useRouter } from 'vue-router';
+import HttpHelper from '../../common/utils/axios_helper';
+import Urls from '../../common/urls/index';
+
+const router = useRouter();
+const toDetail = async () => {
+  router.push('/detail');
+};
+
+// 测试发送请求
+const sendMessage = async () => {
+  const res = await HttpHelper.post(Urls.getDetail, { name: 'test' });
+  console.log(res);
+  alert(JSON.stringify(res));
+};
+
+</script>
+
+<style lang="less"></style>
